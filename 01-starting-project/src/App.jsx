820fa93cae82f4e2bf376e1ff8b2fe87537f62bf
dark-mehul <mehulkumar7858.983524@gmail.com>
2024-05-06 08:56:38 +0530

    first commit

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,48 @@
+import { CORE_CONCEPTS } from './data.js'
+import Header from './components/Header.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
+
+import TabButton from './components/TabButton.jsx';
+
+
+function App() {
+  {/* adding a function for dynamic section of TabButton */}
+  function handleSelect(){
+    console.log('handler function selected');
+  }
+
+  return (
+    <div>
+      <Header></Header>
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[1]}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[2]}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[3]}
+            />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>Jsx</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
